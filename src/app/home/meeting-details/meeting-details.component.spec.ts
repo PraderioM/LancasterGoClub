db1d3e94974906f3922cf89887ef2d81c8f20192
PraderioM <PraderioM@Gmail.com>
2023-02-11 22:33:16 +0000

    Updated to angular 11.

--- src/app/home/meeting-details/meeting-details.component.spec.ts
+++ src/app/home/meeting-details/meeting-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MeetingDetailsComponent } from './meeting-details.component';
 
@@ -6,7 +6,7 @@ describe('MeetingDetailsComponent', () => {
   let component: MeetingDetailsComponent;
   let fixture: ComponentFixture<MeetingDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MeetingDetailsComponent ]
     })
--- src/app/josekle/josekle.component.spec.ts
+++ src/app/josekle/josekle.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { JosekleComponent } from './josekle.component';
 
@@ -6,7 +6,7 @@ describe('JosekleComponent', () => {
   let component: JosekleComponent;
   let fixture: ComponentFixture<JosekleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ JosekleComponent ]
     })
