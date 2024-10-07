69835d129759f4ba71b67b32d66a2a34412dfe82
PraderioM <PraderioM@Gmail.com>
2024-10-07 13:13:59 +0100

    Fixed bug in LUSU page creation.

--- create_lusu_page.sh
+++ create_lusu_page.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-INDEX_FILE_NAME=docs/404.html
+INDEX_FILE_NAME=docs/index.html
 OUTPUT_FILE_NAME=dist/lusu_page.html
 TMP_FILE_NAME=dist/tmp.html
 
 rm $OUTPUT_FILE_NAME
 
 echo 'Building angular project.'
-ng build --configuration production
+ng build --configuration gh-pages
 
 echo 'Building temporary single HTML.'
-inline-to-single-html/venv/bin/python inline-to-single-html/inline_to_single_html.py --output-file $TMP_FILE_NAME $INDEX_FILE_NAME
+inline-to-single-html/.venv/bin/python inline-to-single-html/inline_to_single_html.py --output-file $TMP_FILE_NAME $INDEX_FILE_NAME
 
 echo 'Adding preamble.'
 cat preamble.html $TMP_FILE_NAME >> $OUTPUT_FILE_NAME
